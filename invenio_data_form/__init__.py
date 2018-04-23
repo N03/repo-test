c189e8c6146a0e45271c9b9f180868b002b2486d
Estiocle <noe.zufferey@gmail.com>
2018-04-23 17:13:32 +0200

    ajout du module dans le fichier init

--- invenio_data_form/__init__.py
+++ invenio_data_form/__init__.py
@@ -25,3 +25,8 @@
 """Zenodo deposit additions."""
 
 from __future__ import absolute_import, print_function
+
+from .ext import InvenioDataForm
+from .version import __version__
+
+__all__ = ('__version__', 'InvenioDataForm')
